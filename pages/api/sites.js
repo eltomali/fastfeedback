135ff34d7ed718a4929b38c7dc796e7fbd0e0d31
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-03-15 17:31:23 +0100

    fixes

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -3,10 +3,9 @@ import { auth } from '@/lib/firebase-admin';
 
 export default async (req, res) => {
   try {
-    const token = req.headers.token;
-    const { uid } = await auth.verifyIdToken(token);
-    const sites = await getUserSites(uid);
-    res.status(200).json(sites);
+    const { uid } = await auth.verifyIdToken(req.headers.token);
+    const { sites } = await getUserSites(uid);
+    res.status(200).json({ sites });
   } catch (error) {
     res.status(500).json({ error });
   }

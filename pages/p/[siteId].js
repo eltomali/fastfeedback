a4338a99873ee9a651e072b6ce040f52d392733b
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-03-15 16:23:45 +0100

    stable revalidate

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -13,7 +13,7 @@ export async function getStaticProps(context) {
     props: {
       initialFeedback: feedback
     },
-    unstable_revalidate: 1
+    revalidate: 1
   };
 }
 export async function getStaticPaths() {

c538b2b7096aa87e765dd8ff4ce0285d58446063
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-03-15 15:53:48 +0100

    Using cookies and magic link redirect to dashboard when already logged in

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useAuth } from '@/lib/auth';
-import { Button, Flex, Heading } from '@chakra-ui/react';
+import { Button, Flex, Heading, Link, Text } from '@chakra-ui/react';
 import Head from 'next/head';
 import { FastFeedBackIcon } from 'public/icons';
 
@@ -12,23 +12,49 @@ const Home = () => {
       align="center"
       justify="center"
       h="100vh"
+      maxW="400px"
+      margin="0 auto"
     >
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          if (document.cookie && document.cookie.includes('fast-feedback-auth')) {
+            window.location.href = "/dashboard"
+          }
+        `
+          }}
+        />
         <title>Fast Feedback</title>
       </Head>
 
       <Heading>Fast Feedback</Heading>
-      <FastFeedBackIcon color="black.500" boxSize="64px" />
-      {/* <Text>
-          Current user: <Code>{auth.user ? auth.user.name : 'None'}</Code>
-        </Text> */}
+      <FastFeedBackIcon color="black.500" boxSize="42px" mb={2} />
+      <Text mb={4}>
+        <Text as="span" fontWeight="bold" display="inline">
+          Fast Feedback
+        </Text>
+        {' is being built as part of '}
+        <Link
+          href="https://react20205.com"
+          isExternal
+          textDecoration="underline"
+        >
+          React 2025
+        </Link>
+      </Text>
 
       {auth.user ? (
-        <Button as="a" href="/dashboard">
+        <Button as="a" size="sm" fontWeight="medium" href="/dashboard">
           View Dashboard
         </Button>
       ) : (
-        <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
+        <Button
+          mt={4}
+          size="sm"
+          fontWeight="medium"
+          onClick={(e) => auth.signinWithGitHub()}
+        >
           Sign In
         </Button>
       )}
